624e1a28ed7fda8b7cd9bd24f1558a88f1e44418
Nacho <ignacio.nahuel.ramos@gmail.com>
2022-10-31 15:10:14 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# First i need to input the sudoku board
+# Divide each row in lists
+# Then i need to check if the board is valid
+    # The board is valid if:
+        # Each row has no repeated numbers
+        # Each column has no repeated numbers
+        # Each 3x3 square has no repeated numbers
+    # If it is valid, i need to solve it
+    # If it is not valid, i need to print the error
+# Then i need to print the solved board
+    # First find next empty cell
+    # Check if the value trying to input is valid
+        # Is valid if:
+            # The value is not in the row
+            # The value is not in the column
+            # The value is not in the 3x3 square
+    # Use recursion to solve the board
+    # Store each value in a list
+# Finally print the solved board (stored in the list)
+
